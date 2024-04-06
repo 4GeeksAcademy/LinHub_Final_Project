c4f39704d2341117f5abca9cfa52e4ab57232c28
Luis Miguel Del Valle <150975497+lumi-tip@users.noreply.github.com>
2024-04-06 03:28:21 +0000

    changed section three pic

--- src/front/js/component/home_components/Navbar.js
+++ src/front/js/component/home_components/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useState, useContext } from 'react'
 import { Context } from '../../store/appContext'
-import logo from '../../../img/logo.svg'
 
 const Navbar = () => {
     const [open, setOpen] = useState(true)
@@ -15,16 +14,19 @@ const Navbar = () => {
                     <h1 className='text-gray-500 text-xl font-bold'>LinHub</h1>
                 </div>
                 <div>
-                    <button className='relative font-bold text-gray-500'>Idioma de la pagina: {store.currentIdiom} <span className='text-gray-500'>&#9660;</span></button>
+                    <button onClick={() => setOpen(!open)} className='relative font-bold text-gray-500'>Idioma de la pagina: {store.currentIdiom} <span className='text-gray-500'>&#9660;</span></button>
                     {
                         open &&
-                        <div className='absolute border-slate-500'>
-                            <div className='flex justify-around w-60'>
-                                <div className='text-gray-500 font-bold'>
-                                    <img src='../../../img/BanderaUsa' />
+                        <div className='absolute border-slate-500 top-16'>
+                            <div className='flex justify-evenly w-60'>
+                                <div className='cursor-pointer text-gray-500 font-bold' onClick={() => actions.changeLanguage()}>
+                                    🇺🇸
                                     English
                                 </div>
-                                <div className='text-gray-500 font-bold'>Español</div>
+                                <div className='cursor-pointer text-gray-500 font-bold' onClick={() => actions.changeLanguage()}>
+                                    🇪🇸
+                                    Español
+                                </div>
                             </div>
                         </div>
                     }
