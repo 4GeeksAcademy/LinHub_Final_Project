4a4d31b927d00effb9e2295c47a705a244ca8bc5
Luis Miguel Del Valle <150975497+lumi-tip@users.noreply.github.com>
2024-04-06 01:55:36 +0000

    navbar changes

--- src/front/js/component/home_components/Navbar.js
+++ src/front/js/component/home_components/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react'
-// import logo from '../../../img/logo'
+import logo from '../../../img/logo.svg'
 
 const Navbar = () => {
     return (
         <>
             <nav className='d-flex justify-content-around'>
                 <div>
-                    {/* {logo} */}
+                    <img src={logo} alt='logo de linhub' />
                     <h1>Logo</h1>
                 </div>
                 <h1>drop down</h1>
