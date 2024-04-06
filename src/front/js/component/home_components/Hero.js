59c726d1a35e4df8a592160c8ba3697f03f58fab
Santiago Diaz Muñoz <149324836+Diaz1010@users.noreply.github.com>
2024-04-06 03:44:08 +0000

    oe

--- src/front/js/component/home_components/Hero.js
+++ src/front/js/component/home_components/Hero.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <></>
+        <>
+        <div id='hero-body' className="flex justify-center items-center mt-8">
+
+            <div>
+                <img className='mr-8' src="https://raw.githubusercontent.com/4GeeksAcademy/LinHub_Final_Project/main/src/front/img/section_two.svg" alt="Girl in a jacket" margin-left="800px" width="500" height="600"/>
+            </div>
+
+            <div id='hero' className='d-flex flex-column'  >
+                <h1 className='text-5xl'><strong>! La forma mas divertida y efectiva de aprender ingles!</strong></h1>
+                <button id='boton-hero' type="button" class="btn btn-primary text-center"><strong>Empieza ahora!</strong></button>
+                <button id='boton-hero2' type="button" class="btn btn-primary text-center"><strong>Ya tengo cuenta</strong></button>
+            </div>
+           
+
+        </div>
+       
+        
+        
+        </>
     )
 }
 
