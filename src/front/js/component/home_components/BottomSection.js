2c3d561eff7439935b097fc540d580b60acd4aef
Luis Miguel Del Valle <150975497+lumi-tip@users.noreply.github.com>
2024-04-06 15:10:17 +0000

    added image in section five and startet bottom section

--- src/front/js/component/home_components/BottomSection.js
+++ src/front/js/component/home_components/BottomSection.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import { useContext } from 'react'
+import { Context } from '../../store/appContext'
+
+const UserCard = ({ username, nativeIdiom, learnIdiom }) => {
+
+    const { store, actions } = useContext(Context)
+
+    return (
+        <div className='relative mt-6 min-h-36 border-1'>
+            <div className='flag'></div>
+            <div>
+                <p>{username}</p>
+                <p>{store.currentIdiom == "Español" ? "Quiere aprender: " : "Wants to learn: "} {learnIdiom}</p>
+                <p>{store.currentIdiom == "Español" ? "Nativo en: " : "Native in: "}{nativeIdiom}</p>
+            </div>
+        </div>
+    )
+}
 
 const BottomSection = () => {
     return (
-        <></>
+        <div className='container text-center mt-4'>
+            <h1 className='text-6xl font-bold text-violet-500'>Language Exchange</h1>
+            <div className='flex flex-row justify-around align-center'>
+                <UserCard username={"Diana Roo"} nativeIdiom={"Español"} learnIdiom={"English"} />
+                <img></img>
+                <UserCard username={"Luis Miguel"} nativeIdiom={"English"} learnIdiom={"Spanish"} />
+            </div>
+        </div>
     )
 }
 
