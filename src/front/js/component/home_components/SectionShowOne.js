f8b2df2147a8713d655e8bd8d8e6f8dc5fb278e3
josema-git <152654420+josema-git@users.noreply.github.com>
2024-04-06 04:06:55 +0000

    nothing

--- src/front/js/component/home_components/SectionShowOne.js
+++ src/front/js/component/home_components/SectionShowOne.js
@@ -5,14 +5,8 @@ const SectionShowOne = () => {
     const { store, actions } = useContext(Context);
     return (
         <>
-<<<<<<< HEAD
-        
-        
-        
-        
-=======
             <div className='container flex flex-row justify-around items-center py-4'>
-                {store.currentIdiom === 'Español' ? (
+                {store.language === 'Español' ? (
                     <div className='flex-initial w-50 align-middle'>
                         <h1 className='text-violet-500 text-6xl mb-8 font-bold'>Divertido, Interactivo y gratis</h1>
                         <p className='font-bold text-lg text-gray-500 text-xl'>Sumérgete en nuestras lecciones cortas diseñadas para proporcionarte una experiencia interactiva única. ¡Aquí, no solo aprenderás, sino que también fortalecerás tus habilidades lingüísticas junto a personas reales!</p>
@@ -25,7 +19,6 @@ const SectionShowOne = () => {
                 )}
                 <img className='flex-initial w-30' src='https://raw.githubusercontent.com/4GeeksAcademy/LinHub_Final_Project/main/src/front/img/sectionh_three.svg' />
             </div>
->>>>>>> b980318b918c8057e19bc05695b3987978ab1146
         </>
     )
 }
