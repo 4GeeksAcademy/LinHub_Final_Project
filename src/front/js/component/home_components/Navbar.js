b73e33b9b2efce946322f3905dfae8717b633899
Luis Miguel Del Valle <150975497+lumi-tip@users.noreply.github.com>
2024-04-06 02:19:37 +0000

    tailwind installed

--- src/front/js/component/home_components/Navbar.js
+++ src/front/js/component/home_components/Navbar.js
@@ -4,10 +4,10 @@ import logo from '../../../img/logo.svg'
 const Navbar = () => {
     return (
         <>
-            <nav className='d-flex justify-content-around'>
-                <div>
-                    <img src={logo} alt='logo de linhub' />
-                    <h1>Logo</h1>
+            <nav className='flex bg-red-400'>
+                <div className='flex'>
+                    <img className='' src='https://raw.githubusercontent.com/4GeeksAcademy/LinHub_Final_Project/main/src/front/img/logo.svg' alt='logo de linhub' />
+                    <h1>LinHub</h1>
                 </div>
                 <h1>drop down</h1>
             </nav>
