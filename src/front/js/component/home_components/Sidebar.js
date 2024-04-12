6257a953f41c713dfe3e8020cc04d1daeb361a81
DianaJRoo <149723555+DianaJRoo@users.noreply.github.com>
2024-04-12 02:10:09 +0000

    sidebar

---
+++ src/front/js/component/home_components/Sidebar.js
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';
+import { Context } from '../../store/appContext';
+
+
+export const Sidebar = () => {
+
+    return( <>
+    
+    
+    <h1>hola</h1>
+    
+    
+    </>)
+}
