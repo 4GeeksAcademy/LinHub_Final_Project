3368e0cfca6702f3d87e2183adf7902360f4105d
Santiago Diaz Muñoz <149324836+Diaz1010@users.noreply.github.com>
2024-04-15 19:44:43 +0000

    Lenguage register

--- src/front/js/pages/signUp.js
+++ src/front/js/pages/signUp.js
@@ -19,36 +19,59 @@ export const SignUp = () => {
     setUserInfo({ ...userInfo, [name]: value })
     console.log(userInfo)
   }
+
   const handleSubmit = (e) => {
     e.preventDefault();
     actions.createNewUser(userInfo)
     navigate('/login')
   };
 
-  console.log(userInfo)
-
   return (
-
     <div className='flex flex-col justify-around items-center'>
       <form onSubmit={handleSubmit}>
         <div className=" flex flex-row justify-center my-4">
           <div id="text-form-title" className="text-3xl text-violet-500 ">
             <div className="mt-12">
-              <h1>Ready to learn your dream language?</h1>
-              <p>Let's Start!</p>
+              {store.currentIdiom !== "Español" ? (
+                <>
+                  <h1>Ready to learn your dream language?</h1>
+                  <p>Let's Start!</p>
+                </>
+              ) : (
+                <>
+                  <h1>Listo para aprender tu idioma de ensueño?</h1>
+                  <p>¡Empecemos!</p>
+                </>
+              )}
             </div>
           </div>
           <img id="pingui-form" className="" src="https://raw.githubusercontent.com/4GeeksAcademy/LinHub_Final_Project/main/src/front/img/section_two.svg" alt="LinHub Penguin (Logo)" margin-left="800px" width="150px" />
 
         </div>
         <div id="form1" className="border border-gray-900/10 pb-15 p-5 rounded-lg shadow-2">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
-          <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
+          <h2 className="text-base font-semibold leading-7 text-gray-900">
+            {store.currentIdiom !== "Español" ? (
+              <>Personal Information</>
+            ) : (
+              <>Información Personal</>
+            )}
+          </h2>
+          <p className="mt-1 text-sm leading-6 text-gray-600">
+            {store.currentIdiom !== "Español" ? (
+              <>Use a permanent address where you can receive mail.</>
+            ) : (
+              <>Utilice una dirección permanente donde pueda recibir correo.</>
+            )}
+          </p>
 
           <div className="">
             <div className="">
               <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
-                First name
+                {store.currentIdiom !== "Español" ? (
+                  <>First name</>
+                ) : (
+                  <>Nombre</>
+                )}
               </label>
               <div className="mt-2">
                 <input
@@ -64,7 +87,11 @@ export const SignUp = () => {
 
             <div className="">
               <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
-                Last name
+                {store.currentIdiom !== "Español" ? (
+                  <>Last name</>
+                ) : (
+                  <>Apellido</>
+                )}
               </label>
               <div className="mt-2">
                 <input
@@ -80,7 +107,11 @@ export const SignUp = () => {
 
             <div className="sm:col-span-3">
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
+                {store.currentIdiom !== "Español" ? (
+                  <>Email address</>
+                ) : (
+                  <>Dirección de correo electrónico</>
+                )}
               </label>
               <div className="mt-2">
                 <input
@@ -97,7 +128,11 @@ export const SignUp = () => {
             <div>
               <div className="flex items-center justify-between ">
                 <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Password
+                  {store.currentIdiom !== "Español" ? (
+                    <>Password</>
+                  ) : (
+                    <>Contraseña</>
+                  )}
                 </label>
               </div>
               <div className="mt-2">
@@ -116,7 +151,11 @@ export const SignUp = () => {
             <div className="mt-5 flex justify-between gap-6 w-full h-full" >
               <div className="grow">
                 <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900 w-100 ">
-                  First Language
+                  {store.currentIdiom !== "Español" ? (
+                    <>First Language</>
+                  ) : (
+                    <>Idioma Nativo</>
+                  )}
                 </label>
                 <div className="mt-2">
                   <select
@@ -126,14 +165,27 @@ export const SignUp = () => {
                     onChange={handleChange}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6 w-full"
                   >
-                    <option>Español</option>
-                    <option>English</option>
+                    {store.currentIdiom !== "Español" ? (
+                      <>
+                        <option>Spanish</option>
+                        <option>English</option>
+                      </>
+                    ) : (
+                      <>
+                        <option>Español</option>
+                        <option>English</option>
+                      </>
+                    )}
                   </select>
                 </div>
               </div>
               <div className="grow">
                 <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
-                  What do You Want to learn?
+                  {store.currentIdiom !== "Español" ? (
+                    <>What do You Want to learn?</>
+                  ) : (
+                    <>¿Qué Idioma Quieres Aprender?</>
+                  )}
                 </label>
                 <div className="mt-2">
                   <select
@@ -143,10 +195,18 @@ export const SignUp = () => {
                     onChange={handleChange}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                   >
-                    <option>English</option>
-                    <option>Español</option>
+                    {store.currentIdiom !== "Español" ? (
+                      <>
+                        <option>English</option>
+                        <option>Spanish</option>
+                      </>
+                    ) : (
+                      <>
+                        <option>English</option>
+                        <option>Español</option>
+                      </>
+                    )}
                   </select>
-
                 </div>
 
               </div>
@@ -157,12 +217,18 @@ export const SignUp = () => {
               <button className="flex justify-center mt-7 mb-1 bg-purple-900 hover:bg-purple-700 text-purple-200 hover:text-purple-50 font-overpass text-base font-semibold py-3 px-10 rounded "
                 type="submit"
                 title="Ingresar"
-                name="Ingresar">Register</button>
+                name="Ingresar">
+                {store.currentIdiom !== "Español" ? (
+                  <>Register</>
+                ) : (
+                  <>Registrar</>
+                )}
+              </button>
 
             </div>
           </div >
         </div >
       </form >
     </div >
-  )
+  );
 }
