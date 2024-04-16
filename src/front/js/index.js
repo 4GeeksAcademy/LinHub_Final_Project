a386d014d56055f0f28e15d6dd2e1ef056446f96
josema-git <152654420+josema-git@users.noreply.github.com>
2024-04-16 22:19:23 +0000

    stepper

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -1,12 +1,7 @@
-//import react into the bundle
 import React from "react";
-import ReactDOM from "react-dom";
-
-//include your index.scss file into the bundle
+import { createRoot } from "react-dom/client";
 import "../styles/index.css";
-
-//import your own components
 import Layout from "./layout";
 
-//render your react application
-ReactDOM.render(<Layout />, document.querySelector("#app"));
+// render your react application
+createRoot(document.querySelector("#app")).render(<Layout />);
--- src/front/js/pages/exercises/exercise.js
+++ src/front/js/pages/exercises/exercise.js
@@ -1,25 +1,173 @@
-import ProgressMobileStepper from "../../component/stepper";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/material/styles';
+import MobileStepper from '@mui/material/MobileStepper';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 
-function Exercise() {
-    return (
-        <div className="flex justify-between h-screen p-8">
-            <div className="w-full md:w-1/3 bg-gray-200 p-4">
-                <h2 className="text-lg font-semibold">Lado Izquierdo</h2>
-                <div className="border border-gray-300 p-4 rounded-md shadow-lg mt-20">
-                    <h2 className="text-lg font-semibold mb-2">
-                        <ProgressMobileStepper />
-                    </h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra enim nec tincidunt hendrerit.</p>
-                </div>
+const steps = ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+
+export function Exercise() {
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [skipped, setSkipped] = React.useState(new Set());
+  const [selectedOption, setSelectedOption] = React.useState(null);
+  const [answerIsCorrect, setAnswerIsCorrect] = React.useState(false);
+  const theme = useTheme();
+
+  const isStepOptional = (step) => {
+    return step === 1;
+  };
+
+  const isStepSkipped = (step) => {
+    return skipped.has(step);
+  };
+
+  const handleNext = () => {
+    let newSkipped = skipped;
+    if (isStepSkipped(activeStep)) {
+      newSkipped = new Set(newSkipped.values());
+      newSkipped.delete(activeStep);
+    }
+
+    if (answerIsCorrect === true) {
+
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped(newSkipped);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleSkip = () => {
+    if (!isStepOptional(activeStep)) {
+      // You probably want to guard against something like this,
+      // it should never occur unless someone's actively trying to break something.
+      throw new Error("You can't skip a step that isn't optional.");
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped((prevSkipped) => {
+      const newSkipped = new Set(prevSkipped.values());
+      newSkipped.add(activeStep);
+      return newSkipped;
+    });
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+
+
+  const handleOptionSelect = (option) => {
+    setSelectedOption(option);
+  };
+
+  return (
+    <div className="flex justify-center">
+      <div className="w-100">
+        <Stepper activeStep={activeStep}>
+          <MobileStepper
+            className="w-full"
+            variant="progress"
+            steps={6}
+            position="static"
+            activeStep={activeStep}
+            sx={{ maxWidth: '100%', flexGrow: 1 }} // Modified width to be 100%
+            nextButton={
+              <Button size="small" onClick={handleNext} disabled={activeStep === 5}>
+                Next
+                {theme.direction === 'rtl' ? (
+                  <KeyboardArrowLeft />
+                ) : (
+                  <KeyboardArrowRight />
+                )}
+              </Button>
+            }
+            backButton={
+              <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                {theme.direction === 'rtl' ? (
+                  <KeyboardArrowRight />
+                ) : (
+                  <KeyboardArrowLeft />
+                )}
+                Back
+              </Button>
+            }
+          />
+          {steps.map((label, index) => {
+            const stepProps = {};
+            const labelProps = {};
+            if (isStepOptional(index)) {
+              labelProps.optional = (
+                <Typography variant="caption">Optional</Typography>
+              );
+            }
+            if (isStepSkipped(index)) {
+              stepProps.completed = false;
+            }
+            return (
+              <Step key={label} {...stepProps}>
+              </Step>
+            );
+          })}
+        </Stepper>
+        {activeStep === steps.length ? (
+          <React.Fragment>
+            <Typography sx={{ mt: 2, mb: 1 }}>
+              All steps completed - you&apos;re finished
+            </Typography>
+            <div className="flex flex-row pt-2">
+              <div className="flex-1"></div>
+              <Button onClick={handleReset}>Reset</Button>
             </div>
-            <div className="hidden md:block w-2/3 bg-white-300 p-4">
-                <h2 className="text-lg font-semibold">Lado Derecho</h2>
-                <div className="relative h-4 bg-gray-200 rounded-full overflow-hidden flex">
-                    <div className="absolute left-0 top-0 h-full bg-purple-200 animate-progress-stripes" style={{ width: '65%' }}></div>
-                </div>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <Typography sx={{ mt: 2, mb: 1 }}>step:{activeStep + 1}</Typography>
+            <div className="flex flex-wrap justify-center">
+              <img src="https://via.placeholder.com/150" alt="dog" className="m-2" />
+              <img src="https://via.placeholder.com/150" alt="dog" className="m-2" />
+              <img src="https://via.placeholder.com/150" alt="dog" className="m-2" />
+              <img src="https://via.placeholder.com/150" alt="dog" className="m-2" />
             </div>
-        </div>
-    );
+            <div className="flex justify-center mt-4">
+              <Button
+                variant={selectedOption === 1 ? 'contained' : 'outlined'}
+                onClick={() => { handleOptionSelect(1); setAnswerIsCorrect(true); }}
+              >
+                Option 1
+              </Button>
+              <Button
+                variant={selectedOption === 2 ? 'contained' : 'outlined'}
+                onClick={() => { handleOptionSelect(2); setAnswerIsCorrect(false); }}
+              >
+                Option 2
+              </Button>
+              <Button
+                variant={selectedOption === 3 ? 'contained' : 'outlined'}
+                onClick={() => { handleOptionSelect(3); setAnswerIsCorrect(false); }}
+              >
+                Option 3
+              </Button>
+              <Button
+                variant={selectedOption === 4 ? 'contained' : 'outlined'}
+                onClick={() => { handleOptionSelect(4); setAnswerIsCorrect(false); }}
+              >
+                Option 4
+              </Button>
+            </div>
+          </React.Fragment>
+        )}
+      </div>
+    </div >
+  );
 }
-
-export { Exercise };
