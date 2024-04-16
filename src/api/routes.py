17a71aaaaab868ec9b8c88ae6890ea290ddf82fc
josema-git <152654420+josema-git@users.noreply.github.com>
2024-04-16 01:18:33 +0000

    .

--- src/api/routes.py
+++ src/api/routes.py
@@ -20,14 +20,6 @@ languages = [
     }
 ]
 
-# @api.route('/idioms', methods=['POST'])
-# def post_idioms():
-#     for idiom in idioms:
-#         new_idiom = Idiom(idiom_name=idiom["idiom_name"])
-#         db.session.add(new_idiom)
-#     db.session.commit()
-#     return jsonify({"msg": "Idioms created successfully"}), 200
-
 @api.route('/users', methods=['GET'])
 def get_users():
     users = User.query.all()
@@ -46,38 +38,38 @@ def get_languages():
     languages = Language.query.all()
     languages = list(map(lambda x: x.serialize(), languages))
     return jsonify(languages), 200
-S
-# @api.route('/users', methods=['POST'])
-# def create_user():
-#     request_body = request.get_json()
-#     if not request_body:
-#         return jsonify({"msg": "No data provided"}), 400
 
+
+@api.route('/users', methods=['POST'])
+def create_user():
+    request_body = request.get_json()
+    if not request_body:
+        return jsonify({"msg": "No data provided"}), 400
 
     first_name = request_body.get('first_name')
     last_name = request_body.get('last_name')
     email = request_body.get('email')
     password = request_body.get('password')
-    learning_language = Idiom.query.filter_by(idiom_name=request_body.get("learning_language")).first()
-    native_language = Idiom.query.filter_by(idiom_name=request_body.get("native_language")).first()
+    learning_language = Language.query.filter_by(Language_name=request_body.get("learning_language")).first()
+    native_language = Language.query.filter_by(Language_name=request_body.get("native_language")).first()
     is_active = True
 
-#     if not first_name or not last_name:
-#         return jsonify({"msg": "First name and last name are required"}), 400
+    if not first_name or not last_name:
+        return jsonify({"msg": "First name and last name are required"}), 400
 
-#     new_user = User(
-#         first_name=first_name,
-#         last_name=last_name,
-#         email=email,
-#         password=password,
-#         is_active=is_active,
-#         learning_language=learning_language,
-#         native_language=native_language
-#     )
-#     db.session.add(new_user)
-#     try:
-#         db.session.commit()
-#         return jsonify(new_user.serialize()), 201
-#     except Exception as e:
-#         db.session.rollback()
-#         return jsonify({"msg": "Error creating user", "error": str(e)}), 500
+    new_user = User(
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        password=password,
+        is_active=is_active,
+        learning_language=learning_language,
+        native_language=native_language
+    )
+    db.session.add(new_user)
+    try:
+        db.session.commit()
+        return jsonify(new_user.serialize()), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"msg": "Error creating user", "error": str(e)}), 500
