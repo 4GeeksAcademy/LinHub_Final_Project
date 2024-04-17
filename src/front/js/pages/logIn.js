0cdde329fa9731cccd0fbbf5477ed41e86da1b79
Luis Miguel Del Valle <150975497+lumi-tip@users.noreply.github.com>
2024-04-17 18:06:08 +0000

    changes

--- src/front/js/pages/logIn.js
+++ src/front/js/pages/logIn.js
@@ -17,7 +17,7 @@ export const LogIn = () => {
     const handleLogin = async (e) => {
         e.preventDefault()
         const logged = await actions.loginUser(userInfo)
-        if (logged) navigate('/usercourse')
+        if (logged) navigate(`/usercourse/${userInfo.username}`)
         else setError(true)
     }
 
