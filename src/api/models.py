33a6b8365e88217d4e943d9eca2b3c1c4e5ed1bc
Luis Miguel Del Valle <150975497+lumi-tip@users.noreply.github.com>
2024-04-17 04:52:34 +0000

    changes in routes

--- src/api/models.py
+++ src/api/models.py
@@ -10,7 +10,7 @@ class User(db.Model):
     last_name = db.Column(db.String(120), unique=False, nullable=False)
     username = db.Column(db.String(120), unique = True, nullable = False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(200), unique=False, nullable=False)
 
     # user native language
     learning_language_id = db.Column(db.Integer, db.ForeignKey('languages.id'))
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,10 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User, Language
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+import bcrypt
 
 
 api = Blueprint('api', __name__)
@@ -41,7 +45,7 @@ def get_languages():
     return jsonify(languages), 200
 
 
-@api.route('/users', methods=['POST'])
+@api.route('/register', methods=['POST'])
 def create_user():
     request_body = request.get_json()
     if not request_body:
@@ -68,18 +72,29 @@ def create_user():
     if not password:
         return jsonify({'msg': 'password required'})
     
-    # if not learning_language:
-    #     return jsonify({'msg': 'learning language required'})
+    if not learning_language:
+        return jsonify({'msg': 'learning language required'})
     
-    # if not native_language:
-    #     return jsonify({'msg': 'native language required'})
+    if not native_language:
+        return jsonify({'msg': 'native language required'})
+    
+    password_in_bytes = bytes(password, 'utf-8')
+
+    # Generating Salt
+    salt = bcrypt.gensalt()
+
+    # Hashing Password
+    hash_password = bcrypt.hashpw(    
+        password=password_in_bytes,
+        salt=salt
+    )
 
     new_user = User(
         first_name=first_name,
         last_name=last_name,
         username=username,
         email=email,
-        password=password,
+        password=hash_password,
         is_active=is_active,
         learning_language=learning_language,
         native_language=native_language
@@ -88,7 +103,30 @@ def create_user():
     db.session.add(new_user)
     try:
         db.session.commit()
-        return jsonify(new_user.serialize()), 201
+        return jsonify([new_user.serialize(), {"hash": hash_password.decode('utf-8')}]), 201
     except Exception as e:
         db.session.rollback()
         return jsonify({"msg": "Error creating user", "error": str(e)}), 500
+    
+@api.route('/login', methods=['POST'])
+def login():
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+
+    if not email or not password:
+        return jsonify({'msg': 'email and password needed'})
+    
+    user = User.query.filter_by(email = email).one_or_none()
+    
+    if user != None:
+        check = bcrypt.checkpw(
+            password = bytes(password, 'utf-8'),
+            hashed_password = bytes(user.password, 'utf-8')
+        )
+        if check:
+            access_token = create_access_token(identity=email)
+            return jsonify(access_token = access_token)
+        else:
+            return jsonify({'msg': 'wrong password'})
+    else:
+        return jsonify({'msg': 'user not found'})
