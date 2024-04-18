54876955ac588bfc7a027c62b4c1d96acca5a21b
Diana Jimenez Roo <149723555+DianaJRoo@users.noreply.github.com>
2024-04-18 02:11:00 +0000

    userprofile

--- src/front/js/pages/userprofile.js
+++ src/front/js/pages/userprofile.js
@@ -1,18 +1,56 @@
 import React from 'react';
-import { Context } from "../store/appContext";
 
 
 
-export const UserProfile= () => {
-    const { store, actions } = useContext(Context);
+export const UserProfile = () => {
+    
     return (<>
-    
-    
-    
-    
-    
-    
-    
+        <div className='flex justify-around item-center mt-8 shadow-3 mx-80'>
+            <form>
+                <div class="space-y-20">
+                    <div class="border-b border-gray-900/10 pb-20">
+                        <h2 class="text-base font-semibold leading-7 text-gray-900">Profile</h2>
+                        <div class="col-span-full">
+                            <label for="photo" class="block text-sm font-medium leading-6 text-gray-900">Photo</label>
+                            <div class="mt-2 flex items-center gap-x-5">
+                                <svg class="h-20 w-20 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                                    <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd" />
+                                </svg>
+                                <button type="button" class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Change</button>
+                            </div>
+                        </div>
+                        <div class="mt-1">
+                            <div class="sm:col-span-4">
+                                <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
+                                <div class="mt-2">
+                                    <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                        <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span>
+                                        <input type="text" name="username" id="username" autocomplete="username" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="janesmith" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mt-1">
+                                <div class="sm:col-span-3">
+                                    <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">First name</label>
+                                    <div class="mt-2">
+                                        <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                    </div>
+                                </div>
+                                <div class="sm:col-span-3">
+                                    <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Last name</label>
+                                    <div class="mt-2">
+                                        <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                    </div>
+                                </div>
+                                <div class="mt-6 flex items-center justify-end gap-x-6">
+                                    <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+                                    <button type="submit" class="rounded-md bg-purple-900 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
     </>)
-
 }
