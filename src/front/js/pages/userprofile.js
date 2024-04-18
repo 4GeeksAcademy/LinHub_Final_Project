44b859971bf28d4af20deb2e0333e64b7bc0f373
Diana Jimenez Roo <149723555+DianaJRoo@users.noreply.github.com>
2024-04-18 03:01:50 +0000

    userprofile

--- src/front/js/pages/userprofile.js
+++ src/front/js/pages/userprofile.js
@@ -1,9 +1,9 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import { Context } from "../store/appContext";
 
 
 export const UserProfile = () => {
-    
+    const { store, actions } = useContext(Context);
     return (<>
         <div className='flex justify-around item-center mt-8 shadow-3 mx-80'>
             <form>
@@ -21,35 +21,69 @@ export const UserProfile = () => {
                         </div>
                         <div class="mt-1">
                             <div class="sm:col-span-4">
-                                <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
-                                <div class="mt-2">
-                                    <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                        <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span>
-                                        <input type="text" name="username" id="username" autocomplete="username" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="janesmith" />
-                                    </div>
+                                <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                                    User name
+                                </label>
+                                <div className="">
+                                    <input
+                                        type="text"
+                                        name="username"
+                                        id="username"
+                                        autoComplete="family-name"
+                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
                                 </div>
-                            </div>
-                            <div class="mt-1">
-                                <div class="sm:col-span-3">
-                                    <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">First name</label>
-                                    <div class="mt-2">
-                                        <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                                    </div>
                                 </div>
-                                <div class="sm:col-span-3">
-                                    <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Last name</label>
-                                    <div class="mt-2">
-                                        <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                <div class="mt-1">
+                                    <div class="sm:col-span-3">
+                                        <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+                                            {store.currentIdiom !== "Español" ? (
+                                                <>First name</>
+                                            ) : (
+                                                <>Nombre</>
+                                            )}
+                                        </label>
+                                        <div className="">
+                                            <input
+                                                type="text"
+                                                name="first_name"
+                                                id="first-name"
+                                                autoComplete="family-name"
+                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            />
+                                        </div>
+                           
+
+
                                     </div>
-                                </div>
-                                <div class="mt-6 flex items-center justify-end gap-x-6">
-                                    <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-                                    <button type="submit" class="rounded-md bg-purple-900 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+                                    <div class="sm:col-span-3">
+                                        <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
+                                            {store.currentIdiom !== "Español" ? (
+                                                <>Last name</>
+                                            ) : (
+                                                <>Apellido</>
+                                            )}
+                                        </label>
+                                        <div className="">
+                                            <input
+                                                type="text"
+                                                name="last_name"
+                                                id="last-name"
+                                                autoComplete="family-name"
+                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            />
+                                        </div>
+
+                                    </div>
+
+                                    <div class="mt-6 flex items-center justify-end gap-x-6">
+                                        <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+                                        <button type="submit" class="rounded-md bg-purple-900 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
             </form>
         </div>
     </>)
