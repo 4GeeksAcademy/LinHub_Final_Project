b948e0cc86fbd94bcc45ea97f8eb30b6320583ed
Diana Jimenez Roo <149723555+DianaJRoo@users.noreply.github.com>
2024-04-18 03:56:23 +0000

    userprofile

--- src/front/js/pages/userprofile.js
+++ src/front/js/pages/userprofile.js
@@ -5,24 +5,52 @@ import { Context } from "../store/appContext";
 export const UserProfile = () => {
     const { store, actions } = useContext(Context);
     return (<>
-        <div className='flex justify-around item-center mt-8 shadow-3 mx-80'>
+        <div className='mx-auto max-w-screen-lg px-4 sm:px-6 lg:px-6 py-6 shadow-3 bg-gradient-to-br from-indigo-100 to-purple-400'>
             <form>
-                <div class="space-y-20">
+                <div class="space-y-40 mt-3">
                     <div class="border-b border-gray-900/10 pb-20">
-                        <h2 class="text-base font-semibold leading-7 text-gray-900">Profile</h2>
+                        <h2 class="text-base font-semibold leading-7 text-gray-900">
+                            {store.currentIdiom !== "Español" ? (
+                                <>Profile</>
+                            ) : (
+                                <>Perfil</>
+                            )}
+
+                        </h2>
+
                         <div class="col-span-full">
-                            <label for="photo" class="block text-sm font-medium leading-6 text-gray-900">Photo</label>
-                            <div class="mt-2 flex items-center gap-x-5">
-                                <svg class="h-20 w-20 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                            <label for="photo" class="flex justify-center block text-sm font-medium leading-6 text-gray-900">
+                                {store.currentIdiom !== "Español" ? (
+                                    <>Photo</>
+                                ) : (
+                                    <>Foto</>
+                                )}
+
+
+
+                            </label>
+                            <div class="mt-2 flex justify-center items-center gap-x-5">
+                                <svg class="h-40 w-40 text-gray-600" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
                                     <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd" />
                                 </svg>
-                                <button type="button" class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Change</button>
+                                <button type="button" class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                                    {store.currentIdiom !== "Español" ? (
+                                        <>Change</>
+                                    ) : (
+                                        <>Cambiar</>
+                                    )}
+
+                                </button>
                             </div>
                         </div>
                         <div class="mt-1">
                             <div class="sm:col-span-4">
                                 <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-                                    User name
+                                    {store.currentIdiom !== "Español" ? (
+                                        <>User name</>
+                                    ) : (
+                                        <>Nombre de Usuario</>
+                                    )}
                                 </label>
                                 <div className="">
                                     <input
@@ -33,57 +61,72 @@ export const UserProfile = () => {
                                         className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     />
                                 </div>
+                            </div>
+                            <div class="mt-1">
+                                <div class="sm:col-span-3">
+                                    <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+                                        {store.currentIdiom !== "Español" ? (
+                                            <>First name</>
+                                        ) : (
+                                            <>Nombre</>
+                                        )}
+                                    </label>
+                                    <div className="">
+                                        <input
+                                            type="text"
+                                            name="first_name"
+                                            id="first-name"
+                                            autoComplete="family-name"
+                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        />
+                                    </div>
+
+
+
                                 </div>
-                                <div class="mt-1">
-                                    <div class="sm:col-span-3">
-                                        <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
-                                            {store.currentIdiom !== "Español" ? (
-                                                <>First name</>
-                                            ) : (
-                                                <>Nombre</>
-                                            )}
-                                        </label>
-                                        <div className="">
-                                            <input
-                                                type="text"
-                                                name="first_name"
-                                                id="first-name"
-                                                autoComplete="family-name"
-                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                            />
-                                        </div>
-                           
-
-
-                                    </div>
-                                    <div class="sm:col-span-3">
-                                        <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
-                                            {store.currentIdiom !== "Español" ? (
-                                                <>Last name</>
-                                            ) : (
-                                                <>Apellido</>
-                                            )}
-                                        </label>
-                                        <div className="">
-                                            <input
-                                                type="text"
-                                                name="last_name"
-                                                id="last-name"
-                                                autoComplete="family-name"
-                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                            />
-                                        </div>
-
+                                <div class="sm:col-span-3">
+                                    <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
+                                        {store.currentIdiom !== "Español" ? (
+                                            <>Last name</>
+                                        ) : (
+                                            <>Apellido</>
+                                        )}
+                                    </label>
+                                    <div className="">
+                                        <input
+                                            type="text"
+                                            name="last_name"
+                                            id="last-name"
+                                            autoComplete="family-name"
+                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        />
                                     </div>
 
-                                    <div class="mt-6 flex items-center justify-end gap-x-6">
-                                        <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-                                        <button type="submit" class="rounded-md bg-purple-900 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
-                                    </div>
+                                </div>
+
+                                <div class="mt-6 flex items-center justify-end gap-x-6">
+                                    <button type="button" class="text-sm font-semibold leading-6 text-gray-900">
+                                        {store.currentIdiom !== "Español" ? (
+                                            <>Cancel</>
+                                        ) : (
+                                            <>Cancelar</>
+                                        )}
+
+                                    </button>
+                                    <button type="submit" class="rounded-md bg-purple-900 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                    {store.currentIdiom !== "Español" ? (
+                                            <>Save</>
+                                        ) : (
+                                            <>Guardar</>
+                                        )}
+
+                                    
+                                    </button>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </div>
             </form>
         </div>
     </>)
