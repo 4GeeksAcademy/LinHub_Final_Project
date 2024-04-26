413abae9f9671eb140971346783f843735dbb609
josema-git <152654420+josema-git@users.noreply.github.com>
2024-04-26 21:31:51 +0000

    exercise design

--- src/front/js/pages/exercises/exercise.js
+++ src/front/js/pages/exercises/exercise.js
@@ -19,8 +19,10 @@ export function Exercise() {
     const [selectedOption, setSelectedOption] = React.useState(null);
     const [correctAnswers, setCorrectAnswers] = React.useState(0);
     const [steps, setSteps] = React.useState([]);
+    const [response, setResponse] = React.useState("");
     const theme = useTheme();
     const { id } = useParams();
+
     React.useEffect(() => {
         if (store.userToken) {
             const currentLesson = async () => {
@@ -28,7 +30,6 @@ export function Exercise() {
                     const res = await fetch(process.env.BACKEND_URL + `/api/lesson_questions/${id}`)
                     const data = await res.json();
                     if (!res.ok) throw new Error("Invalid credentials");
-                    console.log(data)
                     setSteps(data)
                 } catch (error) {
                     console.error("Error logging in:", error);
@@ -60,8 +61,8 @@ export function Exercise() {
         } catch (error) {
             console.error("Error checking option:", error);
         }
-
         setSelectedOption(null);
+        setResponse("");
         setActiveStep((prevActiveStep) => prevActiveStep + 1);
     };
 
@@ -72,15 +73,15 @@ export function Exercise() {
 
 
     const handleOptionSelect = (option) => {
-        setSelectedOption(option);
+        setSelectedOption(option.id);
+        setResponse(option.option);
     };
 
     return (
         <div className='w-100'>
             <div className="flex justify-center my-3" >
-                <div className="w-4/12 outlined border-3" style={{ borderColor: 'purple' }}>
+                <div id='colorChange' className="w-1/2 h-auto outlined border-2 p-3 rounded-3xl" style={{ borderColor: 'purple' }}>
                     <Stepper className='flex justify-between' activeStep={activeStep}>
-                        <div></div>
                         <MobileStepper
                             className="w-100"
                             color='purple'
@@ -101,6 +102,7 @@ export function Exercise() {
                             }
                         />
                     </Stepper>
+                    <hr style={{ color: 'purple' }} />
                     {activeStep === Object.keys(steps).length ? (
                         <React.Fragment>
                             <Typography sx={{ mt: 2, mb: 1 }}>
@@ -119,16 +121,18 @@ export function Exercise() {
                         </React.Fragment>
                     ) : (
                         <React.Fragment>
-                            <Typography className='flex justify-center' sx={{ mt: 2, mb: 1, color: 'purple' }}><strong>{steps[activeStep]['question']}</strong></Typography>
+                            <Typography className='flex justify-start' sx={{ mt: 2, mb: 1, color: 'white' }}><h3 className='bg-indigo-500 m-2 p-2 rounded-t-full rounded-r-full'>{steps[activeStep]['question']}</h3></Typography>
+                            <Typography className='flex justify-end' sx={{ mt: 2, mb: 1, color: 'white' }}><h3 className='bg-indigo-500 m-2 p-2 rounded-t-full rounded-l-full'>{response}</h3></Typography>
+                            <hr style={{ color: 'purple' }} />
                             <div className="flex flex-wrap justify-center flex-column">
-                                <div className="flex justify-around mt-4 flex-wrap">
+                                <div className="flex justify-around flex-col mt-4 flex-wrap">
                                     {steps[activeStep]['options'].map(option => (
                                         <Button
-                                            className='my-2'
+                                            className='my-2 rounded-full'
                                             key={option.id}
                                             variant={selectedOption === option.id ? "contained" : "outlined"}
                                             color="primary"
-                                            onClick={() => handleOptionSelect(option.id)}
+                                            onClick={() => handleOptionSelect(option)}
                                             style={{ borderColor: 'purple', color: 'purple' }}
                                         >
                                             {option.option.substring(0, 4) === "http" ? <img src={option.option} alt="option" style={{ width: '100px' }} /> : option.option}
