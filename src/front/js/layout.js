3f8f368eefe09ded75eb551ab5b451d58d47da49
Jose Manuel Sanchez <152654420+josema-git@users.noreply.github.com>
2024-05-05 19:00:40 +0000

    dark mode

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -16,11 +16,16 @@ import { Chat } from "./pages/chats";
 
 import injectContext from "./store/appContext";
 
+
 const darkTheme = createTheme({
     palette: {
         mode: 'dark',
+        text: {
+            primary: '#ffffff',
+        },
     },
 });
+;
 
 //create your first component
 const Layout = () => {
